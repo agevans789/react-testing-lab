235a440979165d33be632f437637c78d2b2a2822
Ann Grace Evans <agevans789@gmail.com>
2025-12-20 15:22:32 -0500

    all tests passing

--- src/__tests__/test_suites/AddTransactions.test.jsx
+++ src/__tests__/test_suites/AddTransactions.test.jsx
@@ -29,7 +29,7 @@ describe('Our app will', () => {
                 ok: true,
                 status: 200,
                 json: () => Promise.resolve(global.transactions || [])
-                }
+                };
             }
         ));
 });
